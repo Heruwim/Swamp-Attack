56c6d05b8e013e60f70d0339f6cbca26d4810e7e
Heruwim <heruwim25@gmail.com>
2023-03-15 14:00:47 +0200

    Added gun and shooting scripts, implemented multiple gun and shotgun

---
+++ Assets/Scripts/Weapon/Pistol.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public override void Shoot(Transform shootPoint)
+    {
+        Instantiate(Bullet, shootPoint.position,Quaternion.identity);
+    }
+}
